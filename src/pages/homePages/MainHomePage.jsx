02f9ec2217cc94b28cc31d6d1366245c1fb67c75
Avinash <palakurthiavinash07@gmail.com>
2024-02-14 19:55:08 +0530

    font size

--- src/pages/homePages/MainHomePage.jsx
+++ src/pages/homePages/MainHomePage.jsx
@@ -5,8 +5,10 @@ const MainHomePage = () => {
 		<div className="bg-black h-screen w-full font-roboto ">
 			<div className="px-4 py-20 mx-auto">
 				<div className="text-center text-white">
-					<h1 className=" font-thin  capitalize md:text-7xl">welcome to </h1>
-					<p className="font-extrabold text-red-600 uppercase md:text-7xl">
+					<h1 className="text-md font-thin  capitalize md:text-7xl">
+						welcome to{" "}
+					</h1>
+					<p className="font-extrabold text-xl text-red-600 uppercase md:text-7xl">
 						demo rpg
 					</p>{" "}
 					<p className="mx-auto mt-4 text-gray-500 capitalize md:text-[1.3rem] md:w-3/5 ">
